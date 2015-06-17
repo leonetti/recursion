7d5a6120be01d94bc10d051246cb57912fbfb150
Alex Leonetti <leonetti.tech@gmail.com>
2015-06-16 17:26:00 -0700

    Have a working function when testing on websites. Still have an error with the test functions.

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -3,8 +3,27 @@
 //   return document.getElementsByClassName(className);
 // };
 
+//document.body
+//element.childNodes
+//element.classList
+
 // But instead we're going to implement it from scratch:
-var getElementsByClassName = function(className
-){
-  // your code here
+var getElementsByClassName = function(className){
+	
+	var elements = [];
+	
+	function checkNestedTags(currentTag, elements, className){
+		var arrayOfTags = currentTag.childNodes;
+		for (var i = 0; i < arrayOfTags.length; i++){
+		    var classes = arrayOfTags[i].classList;
+		    if (classes && classes.contains(className)) {
+		        elements.push(arrayOfTags[i]);
+		    }
+		    if (arrayOfTags[i].childNodes[0]){
+		        checkNestedTags(arrayOfTags[i], elements, className);
+		    }
+		}
+	}
+	checkNestedTags(document.body, elements, className);
+	return elements;
 };
