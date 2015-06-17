4267eca91186eb9c397a81f9436e81e6c58fefaf
Alex Leonetti <leonetti.tech@gmail.com>
2015-06-16 17:44:10 -0700

    Somewhat working code and passes first few test cases.

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -4,5 +4,38 @@
 // but you don't so you're going to write it from scratch:
 
 var stringifyJSON = function(obj) {
-  // your code goes here
+  var string = '';
+  
+  // IF OBJECT IS ARRAY
+  
+  if (Array.isArray(obj)) {
+  	string = '[';
+  	for (var i = 0; i < obj.length; i++) {
+  		// Check for nested Array
+  		if (Array.isArray(obj[i])) {
+  			stringifyJSON(obj[i]);
+  		}
+  		// Check for object already a string
+  		else if (typeof obj[i] === 'string') {
+  			string += ('"' + obj[i] + '"');
+  		} 
+  		// Check for multiple objects in array
+  		else if (obj[i+1] || typeof obj[i+1] === 'number' && obj[i+1] === 0) {
+  			string += obj[i] + ',';
+  		}
+  		//Otherwise add value to the string 
+  		else {
+  			string += obj[i];
+  		}
+  	}
+  	return string + ']';
+  }
+  
+  // IF OBJECT IS A VALUE
+  if (typeof obj === 'string') {
+  	return '"' + obj + '"';
+  } else {
+  	return string + obj;
+  }
+  
 };
